2860132a78a10436143c756e49981e4a9b2e8af0
garado <alexisgarado@gmail.com>
2025-04-06 01:30:16 -0700

    control: spruce up speaker settings
    - fix default speaker label for expansion panel
    - add volume slider dropdown for each speaker

--- astarion/home/programs/ags/src/windows/control/settings/Speaker.ts
+++ astarion/home/programs/ags/src/windows/control/settings/Speaker.ts
@@ -1,5 +1,5 @@
 import { astalify, Gdk, Gtk, Widget } from "astal/gtk4";
-import { Variable, bind, execAsync } from "astal";
+import { Variable, bind, execAsync, timeout } from "astal";
 import { ExpansionPanel } from "@/components/ExpansionPanel.js";
 import AstalWp from "gi://AstalWp";
 import Pango from "gi://Pango?version=1.0";
@@ -17,7 +17,7 @@ const SpeakerWidget_Default = (sink: AstalWp.Endpoint) =>
     child: Widget.Image({
       iconName: "lock-simple-symbolic",
     }),
-    active: bind(sink, "is-default"),
+    active: bind(sink, "is_default"),
     onClicked: () => {
       sink.set_is_default(!sink.is_default);
 
@@ -41,33 +41,63 @@ const SpeakerWidget_Mute = (sink: AstalWp.Endpoint) =>
       iconName: "speaker-x-symbolic",
     }),
     active: bind(sink, "mute"),
-    onClicked: (self) => {
+    onClicked: () => {
       sink.set_mute(!sink.mute);
-      print(self.active);
     },
   });
 
 /**
- * Widget providing controls for a sink. Speaker?
+ * Widget providing controls for a sink.
  */
-const SpeakerWidget = (sink: AstalWp.Endpoint) =>
-  Widget.CenterBox({
-    startWidget: Widget.Label({
-      cursor: Gdk.Cursor.new_from_name("pointer", null),
-      label: `${sink.description}`,
-      ellipsize: Pango.EllipsizeMode.END,
-    }),
-    endWidget: Widget.Box({
-      children: [SpeakerWidget_Mute(sink), SpeakerWidget_Default(sink)],
-    }),
+const SpeakerWidget = (sink: AstalWp.Endpoint) => {
+  const revealerState = Variable(false);
+  let revealerTimer: any = null;
+
+  return Widget.Box({
+    vertical: true,
+    children: [
+      Widget.CenterBox({
+        startWidget: Widget.Label({
+          cursor: Gdk.Cursor.new_from_name("pointer", null),
+          label: `${sink.description}`,
+          ellipsize: Pango.EllipsizeMode.END,
+          onHoverEnter: () => {
+            revealerTimer = timeout(750, () => {
+              revealerState.set(true);
+            });
+          },
+          onHoverLeave: () => {
+            revealerTimer?.cancel();
+          },
+        }),
+        endWidget: Widget.Box({
+          children: [SpeakerWidget_Mute(sink), SpeakerWidget_Default(sink)],
+        }),
+      }),
+      Widget.Revealer({
+        child: Widget.Slider({
+          cssClasses: ["volume"],
+          min: 0,
+          max: 100,
+          value: bind(sink, "volume").as((volume) => volume * 100),
+          onChangeValue: (self, _: any, newValue: number) => {
+            sink.set_volume(newValue / 100.0);
+          },
+        }),
+        transitionType: Gtk.RevealerTransitionType.SLIDE_DOWN,
+        revealChild: bind(revealerState),
+      }),
+    ],
+    onHoverLeave: () => {
+      revealerState.set(false);
+    },
   });
+};
 
 export const Speaker = (globalRevealerState: Variable<boolean>) => {
   return ExpansionPanel({
     icon: "speaker-low-symbolic",
-    label: bind(wp!.audio, "speakers").as(
-      (speakers) => speakers[0]?.description ?? "None",
-    ),
+    label: bind(wp!.audio.default_speaker, "description"),
     children: bind(wp!.audio, "speakers").as((speakers) =>
       speakers.map(SpeakerWidget),
     ),
