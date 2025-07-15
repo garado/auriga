333f543fe4e32d75943008f07501fa9993e4a1b0
garado <alexisgarado@gmail.com>
2025-07-14 20:26:46 -0700

    [chore] (dash:cal) code cleanup, docs for calendar tab schedule pages

--- hosts/astarion/home/programs/ags/src/windows/dash/calendar/schedule/Schedule.ts
+++ hosts/astarion/home/programs/ags/src/windows/dash/calendar/schedule/Schedule.ts
@@ -1,6 +1,22 @@
+/**
+ * █▀ █▀▀ █░█ █▀▀ █▀▄ █░█ █░░ █▀▀
+ * ▄█ █▄▄ █▀█ ██▄ █▄▀ █▄█ █▄▄ ██▄
+ *
+ * A page in the Calendar tab showing upcoming events in a list format.
+ * Very WIP.
+ */
+
+/*****************************************************************************
+ * Imports
+ *****************************************************************************/
+
 import { Gtk, Widget, astalify } from "astal/gtk4";
 import { GLib } from "astal";
 import Calendar from "@/services/Calendar";
+
+/*****************************************************************************
+ * Module-level variables
+ *****************************************************************************/
 
 const WIDGET_SPACING = 20;
 
