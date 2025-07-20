08feab8492238729af6327405f8ba056b8a4be2a
garado <alexisgarado@gmail.com>
2025-07-20 14:00:42 -0700

    [fix] restore post-rebase carnage

--- hosts/astarion/home/programs/ags/src/windows/control/settings/Monitors.ts
+++ hosts/astarion/home/programs/ags/src/windows/control/settings/Monitors.ts
@@ -1,33 +1,28 @@
-/**
- * █▀▄▀█ █▀█ █▄░█ █ ▀█▀ █▀█ █▀█   █▀▀ █▀█ █▄░█ ▀█▀ █▀█ █▀█ █░░
- * █░▀░█ █▄█ █░▀█ █ ░█░ █▄█ █▀▄   █▄▄ █▄█ █░▀█ ░█░ █▀▄ █▄█ █▄▄
- *
- * Hyprland monitor control widget.
- *
- * Currently doesn't do much and only shows the connected monitors.
- */
-
-/*****************************************************************************
- * Imports
- *****************************************************************************/
-
-import { Gdk, Widget } from "astal/gtk4";
+import { Gdk, Gtk, Widget } from "astal/gtk4";
 import { Variable, bind } from "astal";
 import { ExpansionPanel } from "@/components/ExpansionPanel.js";
-import Hyprland, { AstalHyprlandMonitor } from "gi://AstalHyprland";
-
-/*****************************************************************************
- * Module-level variables
- *****************************************************************************/
+import Hyprland from "gi://AstalHyprland";
+import { HoverRevealWrapper } from "@/components/HoverReveal";
 
 const hypr = Hyprland.get_default();
 
-/*****************************************************************************
- * Widget definition
- *****************************************************************************/
+const DisplayModeSettings = () => {
+  return Widget.Box({
+    orientation: Gtk.Orientation.VERTICAL,
+    children: [
+      Widget.Label({
+        label: "Display mode settings",
+        hexpand: true,
+      }),
+    ],
+  });
+};
 
-export const Monitors = (globalRevealerState: Variable<boolean>) => {
-  const MonitorItem = (mon: AstalHyprlandMonitor) =>
+/**
+ * Settings for each individual monitor.
+ */
+const PerMonitorSettings = () => {
+  const MonitorItem = (mon: Hyprland.Monitor) =>
     Widget.Box({
       visible: true,
       cursor: Gdk.Cursor.new_from_name("pointer", null),
@@ -39,12 +34,27 @@ export const Monitors = (globalRevealerState: Variable<boolean>) => {
       onButtonPressed: () => {},
     });
 
+  const MonitorItemRevealer = (mon: Hyprland.Monitor) =>
+    HoverRevealWrapper({
+      mainContent: MonitorItem(mon),
+      childToReveal: Widget.Label({
+        label: "I don't have many settings",
+      }),
+    });
+
+  return Widget.Box({
+    vertical: true,
+    children: bind(hypr, "monitors").as((monitors) =>
+      monitors.map(MonitorItemRevealer),
+    ),
+  });
+};
+
+export const Monitors = (globalRevealerState: Variable<boolean>) => {
   return ExpansionPanel({
     icon: "monitor-symbolic",
     label: bind(hypr, "monitors").as((monitors) => monitors[0].name ?? "None"),
-    children: bind(hypr, "monitors").as((monitors) =>
-      monitors.map(MonitorItem),
-    ),
+    children: [DisplayModeSettings(), PerMonitorSettings()],
     cssClasses: ["monitor"],
     vertical: true,
     globalRevealerState: globalRevealerState,
