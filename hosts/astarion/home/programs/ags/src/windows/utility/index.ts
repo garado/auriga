b217f46f8b1f4e762f7595fbd3f92991e2e7da1a
garado <alexisgarado@gmail.com>
2025-07-30 20:10:04 -0700

    [feat] basic keybinds for utility panel

--- hosts/astarion/home/programs/ags/src/windows/utility/index.ts
+++ hosts/astarion/home/programs/ags/src/windows/utility/index.ts
@@ -13,12 +13,19 @@ import { App, Astal, Gtk, Widget, astalify } from "astal/gtk4";
 
 import { GeminiChat } from "@/windows/utility/GeminiChat";
 import { Tools } from "@/windows/utility/tools";
+import { setupEventController } from "@/utils/EventControllerKeySetup";
 
 /*****************************************************************************
  * Module-level variables
  *****************************************************************************/
 
 const Notebook = astalify(Gtk.Notebook);
+
+const KEYBOARD_SHORTCUTS = {
+  CLOSE_UTILITY: "Escape",
+  SHOW_TOOLS: "h",
+  SHOW_GEMINI: "l",
+} as const;
 
 /*****************************************************************************
  * Widget definitions
@@ -43,6 +50,7 @@ const UtilityPanel = () =>
 
 export default () => {
   const { TOP, LEFT, BOTTOM } = Astal.WindowAnchor;
+  const utilityPanel = UtilityPanel();
 
   return Widget.Window({
     application: App,
@@ -54,11 +62,28 @@ export default () => {
       revealChild: false,
       transitionDuration: 250,
       transitionType: Gtk.RevealerTransitionType.SLIDE_LEFT,
-      child: UtilityPanel(),
+      child: utilityPanel,
     }),
     setup: (self) => {
       // Workaround for revealer bug. https://github.com/wmww/gtk4-layer-shell/issues/60
       self.set_default_size(1, 1);
+
+      setupEventController({
+        name: "UtilityPanel",
+        widget: self,
+        forwardTarget: utilityPanel,
+        binds: {
+          [KEYBOARD_SHORTCUTS.CLOSE_UTILITY]: () => {
+            App.closeWindow("utility");
+          },
+          [KEYBOARD_SHORTCUTS.SHOW_TOOLS]: () => {
+            utilityPanel.set_current_page(0);
+          },
+          [KEYBOARD_SHORTCUTS.SHOW_GEMINI]: () => {
+            utilityPanel.set_current_page(1);
+          },
+        },
+      });
     },
   });
 };
