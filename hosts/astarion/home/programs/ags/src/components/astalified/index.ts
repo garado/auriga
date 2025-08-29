c615d37e333ec4f7c54fc4b5633a230da812aa30
garado <alexisgarado@gmail.com>
2025-08-29 01:41:10 -0700

    feat(component): add prebuilt astalified gtk frame with my preferred styling

---
+++ hosts/astarion/home/programs/ags/src/components/astalified/index.ts
@@ -0,0 +1,14 @@
+/**
+ * ▄▀█ █▀ ▀█▀ ▄▀█ █░░ █ █▀▀ █ █▀▀ █▀▄
+ * █▀█ ▄█ ░█░ █▀█ █▄▄ █ █▀░ █ ██▄ █▄▀
+ *
+ * Default GTK widgets, but they're fully declarative.
+ */
+
+import FrameFactory from "./Frame";
+
+export const Astalified = {
+  Frame: FrameFactory,
+};
+
+export default Astalified;
