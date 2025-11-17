eec658e820cc1de0283cdabc3e25e937eb04b32b
garado <alexisgarado@gmail.com>
2025-11-17 12:54:17 -0800

    feat(ags:maps): ui to indicate if trip endpoint is pinned
    logic not implemented yet. just the ui

--- hosts/astarion/home/programs/ags/src/views/windows/dash/maps/view/sidebar/components/Prediction.ts
+++ hosts/astarion/home/programs/ags/src/views/windows/dash/maps/view/sidebar/components/Prediction.ts
@@ -57,23 +57,42 @@ export const Prediction = (prediction: PlacePrediction) => {
     children: [icon, info],
   });
 
+  const locationPinned = Widget.Revealer({
+    revealChild: false,
+    transitionType: Gtk.RevealerTransitionType.SWING_LEFT,
+    child: Widget.Button({
+      child: Widget.Image({
+        iconName: "push-pin-symbolic",
+      }),
+    }),
+  });
+
   const setPreviewedLocation = () => {
     if (prediction !== controller.previewedLocation) {
       controller.previewedLocation = prediction;
     }
   };
 
-  return Widget.Button({
-    child: predictionWidget,
+  const widget = Widget.CenterBox({
+    startWidget: predictionWidget,
+    endWidget: locationPinned,
     cssClasses: ["place-prediction"],
     hexpand: true,
     halign: Gtk.Align.FILL,
     cursor: Gdk.Cursor.new_from_name("pointer", null),
-    onButtonPressed: () => {
-      const endpoint: ControllerKey = controller.endpointBeingModified;
-      controller[endpoint] = prediction;
+    onButtonPressed: (_self, event) => {
+      if (event && event.get_modifier_state() & Gdk.ModifierType.CONTROL_MASK) {
+        // Ctrl+Click to toggle pin
+        locationPinned.revealChild = !locationPinned.revealChild;
+      } else {
+        // Click to set as endpoint
+        const endpoint: ControllerKey = controller.endpointBeingModified;
+        controller[endpoint] = prediction;
+      }
     },
     onFocusEnter: setPreviewedLocation,
     onHoverEnter: setPreviewedLocation,
   });
+
+  return widget;
 };
