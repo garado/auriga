a9b2400fd296d65ef788c3c90ae1e4b30cd6fe32
garado <alexisgarado@gmail.com>
2025-12-15 14:12:31 -0600

    feat(nvim): get nixcats set up and have it use my own init.lua

--- hosts/astarion/home/nvim/default.nix
+++ hosts/astarion/home/nvim/default.nix
@@ -1,46 +1,143 @@
 
-# █▄░█ █▀▀ █▀█ █░█ █ █▀▄▀█
-# █░▀█ ██▄ █▄█ ▀▄▀ █ █░▀░█
+# █▀▄ █▀█ █▀▀ █▀▄ █▀▀ █▀▀   █▄░█ █░█ █ █▀▄▀█
+# █▄▀ █▀▄ ██▄ █▄▀ █▄█ ██▄ ▄ █░▀█ ▀▄▀ █ █░▀░█
 
-{ pkgs, config, fetchFromGitHub, ... }: {
-  programs.neovim = {
-    enable = true;
+# A neovim configuration managed with NixCats.
+# https://nixcats.org/
 
-    # For TaskWiki
-    withPython3 = true;
-    extraPython3Packages = (ps: with ps; [
-      pynvim
-      tasklib
-      six
-      packaging
-    ]);
+# Instead of using the nvim pkgs from nixpkgs, NixCats lets you define
+# your own custom neovim package. NixCats lets you use Nix to manage nvim
+# dependencies (plugins, LSPs, etc.) while letting you configure everything
+# else in Lua.
 
-  };
+{ config, lib, inputs, ... }: let
+  utils = inputs.nixCats.utils;
+in {
+  imports = [
+    inputs.nixCats.homeModule
+  ];
+  config = {
+    nixCats = {
+      enable = true;
 
-  # Install NvChad
-  xdg.configFile."nvim".source = pkgs.stdenv.mkDerivation {
-    name = "NvChad";
+      addOverlays = [
+        (utils.standardPluginOverlay inputs)
+      ];
 
-    src = pkgs.fetchFromGitHub {
-      owner = "NvChad";
-      repo = "NvChad";
-      rev = "5189f3e32c87478632bb471817db1df13f8b16d1";
-      hash = "sha256-P5TRjg603/7kOVNFC8nXfyciNRLsIeFvKsoRCIwFP3I=";
+      packageNames = [ "dredge.nvim" ];
+
+      luaPath = ./dredge.lua;
+
+      categoryDefinitions.replace = ({ pkgs, settings, categories, extra, name, mkPlugin, ... }@packageDef: {
+        # Define runtime dependencies
+        lspsAndRuntimeDeps = {
+          general = with pkgs; [
+            lazygit
+          ];
+          lua = with pkgs; [
+            lua-language-server
+            stylua
+          ];
+          nix = with pkgs; [
+            nixd
+            alejandra
+          ];
+          go = with pkgs; [
+            gopls
+            delve
+            golint
+            golangci-lint
+            gotools
+            go-tools
+            go
+          ];
+        };
+
+        # Plugins loaded at startup (without packadd)
+        startupPlugins = {
+          general = with pkgs.vimPlugins; [
+            # lazy loading isnt required with a config this small
+            # but as a demo, we do it anyway.
+            lze
+            lzextras
+            snacks-nvim
+            onedark-nvim
+            vim-sleuth
+          ];
+        };
+
+        # Plugins not loaded at startup (loaded with packadd or autocmds)
+        optionalPlugins = {
+          go = with pkgs.vimPlugins; [
+            nvim-dap-go
+          ];
+          lua = with pkgs.vimPlugins; [
+            lazydev-nvim
+          ];
+          general = with pkgs.vimPlugins; [
+            mini-nvim
+            nvim-lspconfig
+            vim-startuptime
+            blink-cmp
+            nvim-treesitter.withAllGrammars
+            lualine-nvim
+            lualine-lsp-progress
+            gitsigns-nvim
+            which-key-nvim
+            nvim-lint
+            conform-nvim
+            nvim-dap
+            nvim-dap-ui
+            nvim-dap-virtual-text
+          ];
+        };
+
+        # Shared libraries to be added to LD_LIBRARY_PATH
+        sharedLibraries = {
+          general = with pkgs; [ ];
+        };
+
+        # Environment variables available at runtime for plugins
+        environmentVariables = {
+          # test = {
+          #   CATTESTVAR = "It worked!";
+          # };
+        };
+
+        # categories of the function you would have passed to withPackages
+        python3.libraries = {
+          # test = [ (_:[]) ];
+        };
+      });
+
+      # Defining custom nvim package
+      packageDefinitions.replace = {
+        "dredge.nvim" = {pkgs, name, ... }: {
+          settings = {
+            wrapRc = false;
+            unwrappedCfgPath = "/home/alexis/Github/dotfiles/hosts/astarion/home/nvim/dredge.lua";
+
+            aliases = [ "vim" "homeVim" "nvim" "dredge" ];
+
+            suffix-path = true;
+            suffix-LD = true;
+
+            # neovim-unwrapped = inputs.neovim-nightly-overlay.packages.${pkgs.stdenv.hostPlatform.system}.neovim;
+
+            hosts.python3.enable = true;
+            hosts.node.enable = true;
+          };
+          categories = {
+            general = true;
+            lua = true;
+            nix = true;
+            go = false;
+          };
+          extra = {
+            nixdExtras.nixpkgs = ''import ${pkgs.path} {}'';
+          };
+        };
+      };
     };
-
-    # Copy custom NvChad config
-    installPhase = ''
-    mkdir -p $out
-    cp -r ./* $out/
-    cd $out/
-    ln -sf /home/alexis/Github/dotfiles/hosts/astarion/home/programs/nvim/nvchad-custom/ $out/lua/custom
-    '';
-
-    # installPhase = ''
-    # mkdir -p $out
-    # cp -r ./* $out/
-    # cd $out/
-    # cp -r ${./nvchad-custom} $out/lua/custom
-    # '';
   };
 }
