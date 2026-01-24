d76d790c8e64e4a5a9f9f75cc8d87107cc8afb64
garado <alexisgarado@gmail.com>
2026-01-24 11:41:56 -0800

    feat(geth): nightly restic backup to blackreach

--- hosts/gethsemane/nixos/configuration.nix
+++ hosts/gethsemane/nixos/configuration.nix
@@ -83,9 +83,9 @@
   services.restic.backups = {
     daily-cloud = {
       initialize = true;
+      repository = "b2:gethsemane";
       passwordFile = config.sops.secrets.restic_pass.path;
       environmentFile = config.sops.secrets.b2_env.path;
-      repository = "b2:gethsemane";
 
       paths = [
         "/home/vessel/Vault/"
@@ -93,6 +93,27 @@
 
       timerConfig = {
         OnCalendar = "03:00";
+        Persistent = true;
+      };
+
+      pruneOpts = [
+        "--keep-daily 7"
+        "--keep-weekly 4"
+        "--keep-monthly 6"
+      ];
+    };
+
+    daily-blackreach = {
+      initialize = true;
+      repository = "/mnt/blackreach/Vault";
+      passwordFile = config.sops.secrets.restic_pass.path;
+
+      paths = [
+        "/home/vessel/Vault"
+      ];
+
+      timerConfig = {
+        OnCalendar = "02:00";
         Persistent = true;
       };
 
