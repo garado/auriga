c21274a24cbd5b661db977798b9afe5a52001336
garado <alexisgarado@gmail.com>
2026-06-19 11:01:42 -0500

    update devshell with typst deps

--- devshell/python-shell.nix
+++ devshell/python-shell.nix
@@ -6,6 +6,9 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
   buildInputs = [
+      pkgs.typst
+      pkgs.poppler_utils
+      pkgs.noto-fonts-color-emoji
       (pkgs.python3.withPackages (ps: with ps; [
         numpy
         requests
@@ -14,12 +17,12 @@ pkgs.mkShell {
         pillow
         shapely
         piexif
+        pypdf
       ]))
     ];
 
   shellHook = ''
     export NIX_DEV_SHELL="Python"
-    # if [ -n "$ZSH_VERSION" ]; then return; fi
-    # exec zsh
+    export TYPST_FONT_PATHS="$HOME/.local/share/fonts:${pkgs.typst}/share/typst/fonts:${pkgs.noto-fonts-color-emoji}/share/fonts/noto"
   '';
 }
