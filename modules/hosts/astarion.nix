e6573ca2518b7e1655f9e9693b769caeb11b61d6
garado <alexisgarado@gmail.com>
2026-09-01 01:11:01 -0500

    refactor(astarion): minimal bootable config

--- modules/hosts/astarion.nix
+++ modules/hosts/astarion.nix
@@ -7,5 +7,18 @@
 { config, inputs, ... }:
 {
   flake.modules.nixos."hosts/astarion" = {
+    imports = [
+      ../../pre-dendritic/hosts/astarion/nixos/hardware-configuration.nix
+    ];
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+    networking.hostName = "astarion";
+    system.stateVersion = "24.11";
+
+    users.users.alexis = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+      initialPassword = "changeme";
+    };
   };
 }
