fee030bc8f04ef192ca73f0e66182bfe4e5ebc0a
garado <alexisgarado@gmail.com>
2026-09-05 00:43:52 -0700

    chore: update caddy hash

--- modules/selfhosted/caddy.nix
+++ modules/selfhosted/caddy.nix
@@ -14,7 +14,7 @@
     let
       caddyWithCloudflare = pkgs.caddy.withPlugins {
         plugins = [ "github.com/caddy-dns/cloudflare@v0.2.3" ];
-        hash = "sha256-cm2FGTXomIPHHiducvno3D0lAHkOgdZwbqGA3ratR+4=";
+        hash = "sha256-iTox1dCA6PiEiT1TIX3QWF64waYQpI/s/XCqIeRQ5Sc=";
       };
     in
     {
