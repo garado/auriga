fb77f0987f122bea468b5f838b26a22c46a228eb
garado <alexisgarado@gmail.com>
2026-09-14 18:43:02 -0700

    hosts(sisyphus): allow 'private' nix cfg for work machines

--- hosts/sisyphus/default.nix
+++ hosts/sisyphus/default.nix
@@ -4,16 +4,21 @@
 # For non-NixOS machines with Nix installed (usually work laptops).
 # These machines can only have home-manager configs.
 
-{ config, ... }:
+{ config, lib, ... }:
 {
   flake.modules.homeManager."hosts/sisyphus" = {
-    imports = with config.flake.modules.homeManager; [
-      git
-      lf
-      nvim
-      tmux
-      zsh
-    ];
+    imports =
+      with config.flake.modules.homeManager;
+      [
+        git
+        lf
+        nvim
+        tmux
+        zsh
+      ]
+
+      # Work-specific settings live in gitignored files
+      ++ lib.optional (builtins.pathExists ./aurora.nix) ./aurora.nix;
 
     home.stateVersion = "24.11";
 
