d870203aaf9a887b6f5b2e31f5888dd5cbc0377f
garado <alexisgarado@gmail.com>
2026-09-14 18:33:24 -0700

    hosts: add new host Sisyphus for non-NixOS machines (nixpkgs/home-manager only)

---
+++ hosts/sisyphus/default.nix
@@ -0,0 +1,23 @@
+# █▀ █ █▀ █▄█ █▀█ █░█ █░█ █▀
+# ▄█ █ ▄█ ░█░ █▀▀ █▀█ █▄█ ▄█
+
+# For non-NixOS machines with Nix installed (usually work laptops).
+# These machines can only have home-manager configs.
+
+{ config, ... }:
+{
+  flake.modules.homeManager."hosts/pkgs-only" = {
+    imports = with config.flake.modules.homeManager; [
+      git
+      lf
+      nvim
+      tmux
+      zsh
+    ];
+
+    home.stateVersion = "24.11";
+
+    # let home-manager manage/upgrade itself
+    programs.home-manager.enable = true;
+  };
+}
--- modules/flake-configuration.nix
+++ modules/flake-configuration.nix
@@ -1,24 +1,19 @@
-# █▀▀ █▀█ █▄░█ █▀▀ █ █▀▀ █░█ █▀█ ▄▀█ ▀█▀ █ █▀█ █▄░█
-# █▄▄ █▄█ █░▀█ █▀░ █ █▄█ █▄█ █▀▄ █▀█ ░█░ █ █▄█ █░▀█
+# █▀▀ █▀█ █▄░█ █▀▀ █ █▀▀   █▀▀ █▀▀ █▄░█ █▀▀ █▀█ ▄▀█ ▀█▀ █▀█ █▀█
+# █▄▄ █▄█ █░▀█ █▀░ █ █▄█   █▄█ ██▄ █░▀█ ██▄ █▀▄ █▀█ ░█░ █▄█ █▀▄
 
-# This turns every `flake.modules.nixos."hosts/*"` into a real
-# `nixosConfigurations.<name>` that nixos-rebuild can use.
+# This file programmatically generates host and home-manager configurations.
+# All of my host/home-manager configs are defined in `//hosts`.
 #
-# The Python equivalent of this (because I really struggle with Nix syntax) is:
+# nixos:
+# - Finds all `flake.modules.nixos."hosts/*"`
+#     - example:    flake.modules.nixos."hosts/astarion" = { ... }
+# - Turns it into `nixosConfigurations.<name>` that nixos-rebuild can use
+#     - example:    nixos-rebuild switch --flake .#astarion
 #
-# ```py
-# hostname = path.removeprefix("hosts/")
-#
-# flake["nixosConfigurations"] = {
-#   hostname: nixos_system(
-#     special_args={"inputs": inputs},
-#     modules=[module],
-#     networking.hostName=hostname,
-#   )
-#   for path, module in config["flake"]["modules"]["nixos"].items()
-#   if path.startswith("hosts/")
-# }
-# ```
+# home-manager:
+# - Finds all `flake.modules.homeManager."hosts/*"`
+#     - example:    flake.modules.homeManager."hosts/pkgs-only" = { ... }
+# - Turns it into `homeConfigurations.<name>` that `home-manager switch` can use
 
 {
   config,
@@ -26,17 +21,31 @@
   lib,
   ...
 }:
+let
+  hostConfigs =
+    modulesByPath: build:
+    lib.mapAttrs' (
+      path: module: lib.nameValuePair (lib.removePrefix "hosts/" path) (build module path)
+    ) (lib.filterAttrs (n: _: lib.hasPrefix "hosts/" n) modulesByPath);
+in
 {
-  flake.nixosConfigurations = lib.mapAttrs' (
-    path: module:
-    lib.nameValuePair (lib.removePrefix "hosts/" path) (
-      inputs.nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        modules = [
-          module
-          { networking.hostName = lib.mkDefault (lib.removePrefix "hosts/" path); }
-        ];
-      }
-    )
-  ) (lib.filterAttrs (n: _: lib.hasPrefix "hosts/" n) config.flake.modules.nixos);
+  flake.nixosConfigurations = hostConfigs config.flake.modules.nixos (
+    module: path:
+    inputs.nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [
+        module
+        { networking.hostName = lib.mkDefault (lib.removePrefix "hosts/" path); }
+      ];
+    }
+  );
+
+  flake.homeConfigurations = hostConfigs config.flake.modules.homeManager (
+    module: _:
+    inputs.home-manager.lib.homeManagerConfiguration {
+      pkgs = import inputs.nixpkgs { system = "x86_64-linux"; };
+      extraSpecialArgs = { inherit inputs; };
+      modules = [ module ];
+    }
+  );
 }
